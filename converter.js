37d7a1bcb97364981999f1dab07894a8946d1106
jakub <jpilawka@concisesoftware.com>
2021-05-31 10:57:38 +0200

    1. Init commit

---
+++ converter.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer')
+const fs = require("fs")
+
+async function printPDF() {
+    try {
+        fs.readFile('./src/pages/2.html', (err, data) => {
+            convert(data)
+        });
+
+    } catch(e) {
+        console.warn(e)
+    }
+}
+
+async function convert(data) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setContent(data.toString());
+    // await page.pdf({format: 'a4' });
+    await page.pdf({path: './src/pages/file1-chromium.pdf', format: 'a4' });
+    // fs.writeFile('file1.pdf', pdf, (err, dat) => console.log(err, dat))
+
+    await browser.close();
+}
+
+printPDF().then(data => console.log(data)).catch(e=>console.error(e))
