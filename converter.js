25d0dc731ee5833f41c7bdcdaa14286173a36c2a
jakub <jpilawka@concisesoftware.com>
2021-05-31 17:28:24 +0200

    1. Added fonts

--- converter.js
+++ converter.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer')
 const fs = require("fs")
-
-async function printPDF() {
+const pages = [1,2,3,4]
+async function printPDF(page) {
     try {
-        fs.readFile('./src/pages/1.html', (err, data) => {
-            convert(data)
+        fs.readFile(`./src/pages/${page}.html`, (err, data) => {
+            convert(data, page)
         });
 
     } catch(e) {
@@ -12,15 +12,15 @@ async function printPDF() {
     }
 }
 
-async function convert(data) {
+async function convert(data, p) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.setContent(data.toString());
     // await page.pdf({format: 'a4' });
-    await page.pdf({path: './src/pages/file1-chromium.pdf', format: 'a4' });
+    await page.pdf({path: `./src/pages/page${p}.pdf`, format: 'a4', printBackground: true });
     // fs.writeFile('file1.pdf', pdf, (err, dat) => console.log(err, dat))
-
+    console.log(`%cPage ${p} created`, "color:green;font-size:14px")
     await browser.close();
 }
 
-printPDF().then(data => console.log(data)).catch(e=>console.error(e))
+pages.forEach(page => printPDF(page).then(data => data).catch(e=>console.error(e)))
