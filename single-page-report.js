efd48890a7c4ac13e06940684d4310f364152469
jakub <jpilawka@concisesoftware.com>
2021-06-14 14:34:46 +0200

    1. Fixes,. fixes, fixes

---
+++ single-page-report.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+const fs = require("fs");
+const path = './src';
+
+const colorPages = ['report'];
+async function printPDF(page, type) {
+    try {
+        const data = await fs.readFileSync(`${path}/${page}.html`)
+        await convert(data, page, type)
+    } catch(e) {
+        console.warn('printPDF error:',e)
+    }
+}
+
+async function convert(data, p, type) {
+    const browser = await puppeteer.launch({ headless: true });
+
+    const page = await browser.newPage();
+    await page.setContent(data.toString(), { waitUntil: 'networkidle2' });
+    await page.evaluateHandle('document.fonts.ready');
+    await page.content();
+
+    await page.pdf({path: `${path}/single(${type}).pdf`, format: 'a4', printBackground: true, preferCSSPageSize: true});
+    await browser.close();
+    return true;
+}
+
+
+async function process(array, type) {
+    return array.map(async page => await printPDF(page, type));
+}
+
+process(colorPages, 'color').then().catch(e => console.error(e));
